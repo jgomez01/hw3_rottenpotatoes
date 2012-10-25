22ae3097452eb4de376f768305cf1d7846b68283
jgomez01 <gomez.jairo@gmail.com>
2012-10-25 09:59:13 -0700

    Version to sent to grader - 25/oct/2012 - 12 M

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,7 +1,6 @@
 # Add a declarative step here for populating the DB with movies.
 require 'date'
 Given /the following movies exist/ do |movies_table|
-  puts movies_table
   movies_table.hashes.each do |movie|
       Movie.create!(movie)
   end
